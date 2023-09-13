992df092b914d17b4d68e0390fcdac5216958a77
Oleg Polikarpov <Vinches2193@yandex.ru>
2023-09-13 17:38:30 +0300

    Add headers, proxy

---
+++ Lessons/headers.py
@@ -0,0 +1,12 @@
+import requests
+from random import choice
+
+url = 'http://httpbin.org/user-agent'
+
+with open('user_agent.txt') as file:
+    lines = file.read().split('\n')
+
+for line in lines:
+    user_agent = {'user-agent': choice(lines)}
+    response = requests.get(url=url, headers=user_agent)
+    print(response.text)
---
+++ Lessons/proxy_lesson.py
@@ -0,0 +1,18 @@
+from random import choice
+import requests
+
+url = 'http://httpbin.org/ip'
+
+with open('proxy.txt') as file:
+    proxy_file = file.read().split('\n')
+    for _ in range(1000):
+        try:
+            ip = choice(proxy_file).strip()
+            proxy = {
+                'http': f'http://{ip}',
+                'https': f'https://{ip}'
+            }
+            response = requests.get(url=url, proxies=proxy)
+            print(response.json(), 'Success connection')
+        except Exception as _ex:
+            continue
